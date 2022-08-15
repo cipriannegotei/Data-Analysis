7a183b802a26df31128f742ae458d40bd22b5314
cipriannegotei <65068222+cipriannegotei@users.noreply.github.com>
2022-08-15 11:17:04 +0300

    Delete Py Portfolio directory

--- Py Portfolio/Mississippi count.py
+++
@@ -1,6 +0,0 @@
-#Mississippi count
-import time
-
-for i in range(0,5):
-    print(i+1, 'Mississippi')
-    time.sleep(1)
--- Py Portfolio/While break.py
+++
@@ -1,8 +0,0 @@
-x = input('numele este:')
-
-while x:
-    if x == 'chupacabra':
-        print('buhuhu')
-        break
-    else:
-        x = input('numele este:')
